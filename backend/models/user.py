beb6a34c8aa4b6bf2556ff8f7cc5b85fbcd66628
Tomek Scierski <tomasz6612@gmail.com>
2022-03-23 13:11:46 +0100

    Add relationship between User and UserSearchSettings

--- backend/models/user.py
+++ backend/models/user.py
@@ -2,10 +2,12 @@ from database import Base
 from sqlalchemy import Column,String,Integer
 from sqlalchemy.orm import relationship
 
+from models.user_search_settings import UserSearchSettings
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer(), primary_key=True)
     username = Column(String(25),nullable=False,unique=True)
     email = Column(String(25),nullable=True,unique=True)
     hashed_password = Column(String(200), nullable=False)
-    # user_settings = relationship("UserSearchSettings")
+    user_settings = relationship("UserSearchSettings")
--- backend/models/user_search_settings.py
+++ backend/models/user_search_settings.py
@@ -6,9 +6,9 @@ from sqlalchemy.schema import ForeignKey
 class UserSearchSettings(Base):
     __tablename__ = "user_search_settings"
     id = Column(Integer(), primary_key=True)
-    # user_id = Column(Integer, ForeignKey("user.id"),unique=True)
 
     country = Column("country",Enum(Country))
     category = Column("category",Enum(Category))
     source = Column("source",Enum(Source))
     language = Column("language",Enum(Language))
+    user_id = Column(Integer, ForeignKey("user.id"),unique=True)
