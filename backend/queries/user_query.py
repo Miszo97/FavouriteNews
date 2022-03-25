107d39ac2cc739eb69221fc2ee67357f55b2d72b
Tomek Scierski <tomasz6612@gmail.com>
2022-03-25 09:01:51 +0100

    update UserQuery

--- backend/queries/user_query.py
+++ backend/queries/user_query.py
@@ -1,23 +1,41 @@
 from models.user import User
+from schemas.user_schema import UserObject
 from sqlalchemy.orm import Session
 
-class UserQuery():
-    def get_user_by_id(user_id):
-        user = db.session.query(User).filter(User.id==user_id).first()
+
+class UserQuery:
+    def create_user(self, db: Session, user: UserObject):
+        new_user = User(
+            id=user.id,
+            username=user.username,
+            email=user.email,
+            hashed_password=user.hashed_password,
+        )
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        return new_user
+
+    def get_users(self, db: Session):
+        users = db.query(User).all()
+        return users
+
+    def get_user_by_id(self, db: Session, user_id: int):
+        user = db.query(User).filter(User.id == user_id).first()
         return user
-    
-    def get_user_by_username(username):
-        user = db.session.query(User).filter(User.username==username).first()
+
+    def get_user_by_username(self, db: Session, username: str):
+        user = db.query(User).filter(User.username == username).first()
         return user
-    
-    def set_email(user_id, email):
-        user = db.session.query(User).filter(User.id == user_id).first()
+
+    def set_email(self, db: Session, user_id: int, email: str):
+        user = db.query(User).filter(User.id == user_id).first()
         user.email = email
         db.session.commit()
         return user
 
-    def set_username(user_id, username):
-        user = db.session.query(User).filter(User.id == user_id).first()
+    def set_username(self, db: Session, user_id: int, username: str):
+        user = db.query(User).filter(User.id == user_id).first()
         user.username = username
         db.session.commit()
         return user
