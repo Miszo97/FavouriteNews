585badac30ed0fdb5b71721878ca3be6fdb7f77d
Tomek Scierski <tomasz6612@gmail.com>
2022-03-25 10:41:24 +0100

    add realationship between User and UserSearchSettings

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,11 +1,12 @@
 from database import Base
-from sqlalchemy import Column,String,Integer
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
+
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = "user"
     id = Column(Integer(), primary_key=True)
-    username = Column(String(25),nullable=False,unique=True)
-    email = Column(String(25),nullable=True,unique=True)
+    username = Column(String(25), nullable=False, unique=True)
+    email = Column(String(25), nullable=True, unique=True)
     hashed_password = Column(String(200), nullable=False)
-    # user_settings = relationship("UserSearchSettings")
+    user_settings = relationship("UserSearchSettings")
--- backend/models/user_search_settings.py
+++ backend/models/user_search_settings.py
@@ -1,14 +1,15 @@
-from sqlalchemy import Column,String,Integer,Enum
-from enums import Category,Country,Language,Source
 from database import Base
+from enums import Category, Country, Language, Source
+from sqlalchemy import Column, Enum, Integer
 from sqlalchemy.schema import ForeignKey
+
 
 class UserSearchSettings(Base):
     __tablename__ = "user_search_settings"
     id = Column(Integer(), primary_key=True)
-    # user_id = Column(Integer, ForeignKey("user.id"),unique=True)
 
-    country = Column("country",Enum(Country))
-    category = Column("category",Enum(Category))
-    source = Column("source",Enum(Source))
-    language = Column("language",Enum(Language))
+    country = Column("country", Enum(Country))
+    category = Column("category", Enum(Category))
+    source = Column("source", Enum(Source))
+    language = Column("language", Enum(Language))
+    user_id = Column(Integer, ForeignKey("user.id"), unique=True)
