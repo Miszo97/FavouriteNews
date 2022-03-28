1d716b119ef684aadbaf9366a0d9b0015dab2edb
Artur S <kol478@gmail.com>
2022-03-28 12:46:36 +0200

    Fix main file (#12)
    * Fix main file
    
    Co-authored-by: Artur <artur.spek.skygate@gmail.com>

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,9 @@
+from app.routers import users
 from fastapi import FastAPI
-
-from fastapi_sqlalchemy import DBSessionMiddleware  # middleware helper
-from backend.app.routers import users
 
 app = FastAPI()
 app.include_router(users.router)
 
-app.add_middleware(DBSessionMiddleware, db_url="postgresql://postgres:postgres@database:5432/postgres")
 
 @app.get("/")
 async def root():
