e1c03ea3285a1eb29a2be3dc7b295bea6d69e3a9
Artur S <kol478@gmail.com>
2022-03-28 12:48:14 +0200

    Add environment variables (#14)
    Co-authored-by: Artur <artur.spek.skygate@gmail.com>

---
+++ backend/settings.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
+ALGORITHM = os.environ.get("ALGORITHM")
+DATABASE_LOGIN = os.environ.get("DATABASE_LOGIN")
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+SECRET_KEY = os.environ.get("SECRET_KEY")
