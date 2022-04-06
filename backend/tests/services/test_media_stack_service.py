5c7ef0f9da6fd2241312f9d2bddad4dc281bfbb7
Artur <artur.spek.skygate@gmail.com>
2022-04-06 09:03:57 +0200

    Use new episodes mode in vcr

--- backend/tests/services/test_media_stack_service.py
+++ backend/tests/services/test_media_stack_service.py
@@ -6,7 +6,7 @@ from settings import MEDIA_STACK_API
 media_stack_service = MediaStackService(api_key=MEDIA_STACK_API)
 
 
-@pytest.mark.vcr()
+@pytest.mark.vcr(record_mode="new_episodes")
 def test_get_articles_by_parameters():
     articles = media_stack_service.get_news_by_parameters(
         country=Country.US,
@@ -23,7 +23,7 @@ def test_get_articles_by_parameters():
     assert Category.GENERAL.value in first_article.values()
 
 
-@pytest.mark.vcr()
+@pytest.mark.vcr(record_mode="new_episodes")
 def test_get_articles_limit():
     articles = media_stack_service.get_news_by_parameters(
         country=Country.US,
