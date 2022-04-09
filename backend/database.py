d614e0606afc7be537a98e3e9297e506819dc987
Artur <artur.spek.skygate@gmail.com>
2022-04-09 18:46:25 +0200

    Refactor code

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,13 @@
-from sqlalchemy.orm import declarative_base,sessionmaker
+from settings import DATABASE_HOST, POSTGRES_LOGIN, POSTGRES_PASSWORD
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine('postgresql://postgres:postgres@database:5432/postgres')
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{POSTGRES_LOGIN}:{POSTGRES_PASSWORD}@{DATABASE_HOST}:5432/postgres"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 Base = declarative_base()
 
-SessionLocal = sessionmaker(bind = engine)
+SessionLocal = sessionmaker(bind=engine)
