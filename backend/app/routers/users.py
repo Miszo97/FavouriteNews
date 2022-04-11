97a7201e5c5f67c174bc867564c730beb8455e08
tomkeee <80860556+tomkeee@users.noreply.github.com>
2022-04-11 15:11:27 +0200

    fix user creation (#39)
    * fix user creation
    
    * minor updates to /users POST

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -2,7 +2,8 @@ from typing import List, Optional
 
 from dependencies.current_user import get_current_user
 from dependencies.database_session import get_db
-from fastapi import APIRouter, Depends, Form
+from fastapi import APIRouter, Depends, Form, status
+from fastapi.responses import JSONResponse
 from models import User
 from queries.user_query import UserQuery
 from queries.user_serach_settings_query import UserSearchSettingsQuery
@@ -23,6 +24,10 @@ async def register(
     email: Optional[str] = Form(None),
     db: Session = Depends(get_db),
 ):
+    if UserQuery().get_user_by_username(db, username):
+        detail = {"error": "username already exists"}
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=detail)
+
     new_user = User(username=username, email=email)
     new_user.hashed_password = get_password_hash(password)
     return UserQuery().create_user(db, new_user)
