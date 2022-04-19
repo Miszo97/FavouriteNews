8dcc779decd6b1ed0c5db1732ac77d25bde64615
Tomek Ścierski <tomasz6612@gmail.com>
2022-04-19 11:58:43 +0200

    add News and Search Settings page

--- frontend/src/components/NewsScreen.js
+++ frontend/src/components/NewsScreen.js
@@ -1,6 +1,69 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+
 const NewsScreen = () => {
-    return (
-        <h2>Home Screen</h2>
-    )
+  const access_token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lMSIsImV4cCI6MTY1MDM2MzI2OX0.DNcF-0_3R0yjMVH9oU_AQaH0jbw2D17efRUv7upczkc";
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  };
+
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function fetchNews() {
+      const { data } = await axios.get(
+        "http://localhost:8000/users/me/followed-articles",
+        config
+      );
+      setNews(data);
     }
-export default NewsScreen
+
+    fetchNews();
+  }, []);
+
+  function handleClick(url) {}
+  return (
+    <Grid
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ marginTop: "5%", marginBottom: "10%" }}
+    >
+      {news.map((article, index) => (
+        <Card
+          style={{ marginTop: "2%" }}
+          sx={{ minWidth: 275 }}
+          key={article.id}
+        >
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {article.title}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {article.author} {article.source}
+            </Typography>
+            <Typography variant="body2">{article.description}</Typography>
+          </CardContent>
+          <CardActions>
+            <a href={article.url}>
+              <Button onClick={handleClick} size="small">
+                Read More
+              </Button>
+            </a>
+          </CardActions>
+        </Card>
+      ))}
+    </Grid>
+  );
+};
+export default NewsScreen;
--- frontend/src/components/SearchSettings.js
+++ frontend/src/components/SearchSettings.js
@@ -1,7 +1,88 @@
-const SearchSettings=()=>{
-    return (
-        <h1>Search Setting</h1>
-    )
-}
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
+
+const SearchSettings = () => {
+  function handleClick(object) {
+    console.log(object);
+  }
+
+  const access_token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lMSIsImV4cCI6MTY1MDM2MzI2OX0.DNcF-0_3R0yjMVH9oU_AQaH0jbw2D17efRUv7upczkc";
+
+  const [settings, setSettings] = useState({});
+
+  useEffect(() => {
+    async function fetchSettings() {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      };
+      const { data } = await axios.get(
+        "http://localhost:8000/users/me/user-search-settings",
+        config
+      );
+      setSettings(data);
+    }
+    fetchSettings();
+  }, []);
+
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ marginTop: "20%" }}
+    >
+      <Box sx={{ width: "50%", maxWidth: 360 }}>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton onClick={handleClick}>
+              <ListItemText primary={`Country`} />
+              <ListItemText align="right" primary={`${settings.country}`} />
+            </ListItemButton>
+          </ListItem>
+
+          <Divider />
+
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemText primary={`Category`} />
+              <ListItemText align="right" primary={`${settings.category}`} />
+            </ListItemButton>
+          </ListItem>
+
+          <Divider />
+
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemText primary={`Source`} />
+              <ListItemText align="right" primary={`${settings.source}`} />
+            </ListItemButton>
+          </ListItem>
+
+          <Divider />
+
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemText primary={`Language`} />
+              <ListItemText align="right" primary={`${settings.language}`} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+    </Grid>
+  );
+};
 
 export default SearchSettings;
