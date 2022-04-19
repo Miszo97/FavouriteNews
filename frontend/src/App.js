1d182b04e54f1fcd87fd02f4e296e97a5f426ca1
Artur <artur.spek.skygate@gmail.com>
2022-04-19 10:17:42 +0200

    Add forms to home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import Header from "./components/Header"
-import { Container } from '@mui/material';
-import { BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Header from "./components/Header";
+import { Container } from "@mui/material";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import NewsScreen from "./components/NewsScreen"
-import SearchSettings from "./components/SearchSettings"
+import NewsScreen from "./components/NewsScreen";
+import SearchSettings from "./components/SearchSettings";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
@@ -13,14 +15,14 @@ function App() {
       <main>
         <Container>
           <Routes>
-          < Route path="/news" element = {<NewsScreen />}/>
-          < Route path="/settings" element = {<SearchSettings />}/>
-
+            <Route path="/news" element={<NewsScreen />} />
+            <Route path="/settings" element={<SearchSettings />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
           </Routes>
         </Container>
       </main>
     </Router>
-
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,38 +1,53 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
 
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import Link from '@mui/material/Link';
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
 
 const Header = () => {
   return (
-    <AppBar position="static" sx={{backgroundColor: '#313C56'}}>
+    <AppBar position="static" sx={{ backgroundColor: "#313C56" }}>
       <Container maxWidth="xxl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, backgroundColor: "inherit", display: { xs: 'none', md: 'flex' } }}>
-
+          <Box
+            sx={{
+              flexGrow: 1,
+              backgroundColor: "inherit",
+              display: { xs: "none", md: "flex" },
+            }}
+          >
             <Link href="/news">
-                <Button
-                sx={{ my: 1, color: 'white', display: 'block' }}
-                >
+              <Button sx={{ my: 1, color: "white", display: "block" }}>
                 News
-                </Button>
+              </Button>
             </Link>
             <Link href="/settings">
-                <Button
-                sx={{ my: 1, color: 'white', display: 'block' }}
-                >
+              <Button sx={{ my: 1, color: "white", display: "block" }}>
                 Search Settings
-                </Button>
+              </Button>
             </Link>
           </Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-                <Button sx={{ my: 2, color: 'white', display: 'block' }}> Login</Button>
-            </Tooltip>
+          <Box
+            sx={{
+              flexGrow: 0,
+              backgroundColor: "inherit",
+              display: { xs: "none", md: "flex" },
+            }}
+          >
+            <Link href="/signin">
+              <Button sx={{ my: 2, color: "white", display: "block" }}>
+                {" "}
+                Login
+              </Button>
+            </Link>
+            <Link href="/signup">
+              <Button sx={{ my: 2, color: "white", display: "block" }}>
+                {" "}
+                Sign Up
+              </Button>
+            </Link>
           </Box>
         </Toolbar>
       </Container>
