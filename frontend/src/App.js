ff8b19e1f4a5c73a84303c57e48f9ec7b56f82fe
Artur S <kol478@gmail.com>
2022-04-22 09:43:38 +0200

    Change header when a user is logged in (#49)
    * Change header when a user is logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,48 @@
 import Header from "./components/Header";
 import { Container } from "@mui/material";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
 
 import NewsScreen from "./components/NewsScreen";
 import SearchSettings from "./components/SearchSettings";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import { UserProvider } from "./userContext";
+
+function getAccessToken() {
+  return localStorage.getItem("access_token");
+}
+
+function getUserName() {
+  return localStorage.getItem("user_name");
+}
 
 function App() {
+  const [accessToken, setAccessToken] = useState(getAccessToken());
+  const [userName, setUserName] = useState(getUserName());
+
+  const values_for_provider = {
+    accessToken,
+    setAccessToken,
+    userName,
+    setUserName,
+  };
+
   return (
     <Router>
-      <Header />
-
-      <main>
-        <Container>
-          <Routes>
-            <Route path="/news" element={<NewsScreen />} />
-            <Route path="/settings" element={<SearchSettings />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-          </Routes>
-        </Container>
-      </main>
+      <UserProvider value={values_for_provider}>
+        <Header isLoggedIn={accessToken} />
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/news" element={<NewsScreen />} />
+              <Route path="/settings" element={<SearchSettings />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+            </Routes>
+          </Container>
+        </main>
+      </UserProvider>
     </Router>
   );
 }
---
+++ frontend/src/userContext.js
@@ -0,0 +1,4 @@
+import React from "react";
+export const UserContext = React.createContext({});
+export const UserProvider = UserContext.Provider;
+export default UserProvider;
